0f9309ebd05f6c987ee254d6fe2ab8e4ac3dea22
Castersorium <xzyu18@fudan.edu.cn>
2023-11-08 14:44:45 +0800

    3Plots, FuAn Curve

--- plotGraph.C
+++ plotGraph.C
@@ -1,34 +1,40 @@
 #include <fstream>
 #include <TGraph.h>
 #include <TCanvas.h>
+#include <TFile.h>
 #include <iostream>
 
 void plotGraph() {
-    std::ifstream file("3x3NTD.txt");
-    if (!file.is_open()) {
-        std::cout << "Error: File could not be opened." << std::endl;
-        return;
+    std::vector<std::string> filenames = {"3x3NTD.txt", "3x1NTD.txt", "19T20.txt"};
+    TFile *file = new TFile("outputFuAnQuXian.root", "RECREATE");
+
+    for (int i = 0; i < filenames.size(); i++) {
+        std::ifstream dataFile(filenames[i]);
+        if (!dataFile.is_open()) {
+            std::cout << "Error: File " << filenames[i] << " could not be opened." << std::endl;
+            continue;
+        }
+
+        std::vector<double> x, y;
+        double tempX, tempY;
+
+        while (dataFile >> tempX >> tempY) {
+            x.push_back(tempX);
+            y.push_back(tempY);
+        }
+
+        dataFile.close();
+
+        TGraph *graph = new TGraph(x.size(), &y[0], &x[0]);
+        graph->SetTitle((filenames[i] + ";I_Bol(A);V_Bol(V)").c_str());
+        graph->SetMarkerStyle(20);
+
+        TCanvas *c1 = new TCanvas(("c" + filenames[i]).c_str(), ("TGraph Example " + std::to_string(i+1)).c_str(), 200, 10, 700, 500);
+        graph->Draw("APL");
+
+        // Save the canvas to the ROOT file
+        c1->Write();
     }
 
-    std::vector<double> x, y;
-    double tempX, tempY;
-
-    while (file >> tempX >> tempY) {
-        x.push_back(tempX);
-        y.push_back(tempY);
-    }
-
-    file.close();
-
-    TGraph *graph = new TGraph(x.size(), &x[0], &y[0]);
-    //Temp: 35.86 ~ 39.23 mK
-    graph->SetTitle("35.86-39.23 mK, 3x3NTD;V_Bol(V);I_Bol(A)");
-    graph->SetMarkerStyle(20);
-
-    TCanvas *c1 = new TCanvas("c1", "TGraph Example", 200, 10, 700, 500);
-    graph->Draw("APL");
-
-    // Save the canvas to a ROOT file
-    c1->SaveAs("output.root");
-
+    file->Close();
 }
