ec486edaaf04bc4468085304d65a78ec0ac2d4a0
Castersorium <xzyu18@fudan.edu.cn>
2023-11-09 20:02:35 +0800

    Different Temp, easier to change in code

--- src/plotGraph.C
+++ src/plotGraph.C
@@ -11,21 +11,30 @@
 void plotGraph() {
     gStyle->SetOptFit(111);
 
+    std::string dataPath = "../data/";
+    std::string outputPath = "../output/";
+    std::string runName = "2023Nov/";
+    std::string temperature = "29p9mK/";
+
     std::vector<std::string> filenames = {
-        "../data/2023Nov/20p0mK/3x3NTD.txt", 
-        "../data/2023Nov/20p0mK/3x1NTD.txt",
-        "../data/2023Nov/20p0mK/19T20.txt"
-        };
-    TFile *file = new TFile("../output/2023Nov/20p0mK/outputFit.root", "RECREATE");
+        "3x3NTD.txt", 
+        "3x1NTD.txt",
+        "19T20.txt"
+    };
+
+    dataPath = dataPath+runName+temperature;
+    outputPath = outputPath+runName+temperature;
+
+    TFile *file = new TFile((outputPath+"outputFit.root").c_str(), "RECREATE");
     
     // Create a new canvas for the PDF
     TCanvas *cAll = new TCanvas("cAll", "All Graphs", 200, 10, 700, 500);
-    cAll->Print("../output/2023Nov/20p0mK/allGraphs.pdf["); // Open the PDF file
+    cAll->Print((outputPath+"allGraphs.pdf[").c_str()); // Open the PDF file
 
     for (int i = 0; i < filenames.size(); i++) {
-        std::ifstream dataFile(filenames[i]);
+        std::ifstream dataFile(dataPath+filenames[i]);
         if (!dataFile.is_open()) {
-            std::cout << "Error: File " << filenames[i] << " could not be opened." << std::endl;
+            std::cout << "Error: File " << dataPath+filenames[i] << " could not be opened." << std::endl;
             continue;
         }
 
@@ -40,25 +49,29 @@ void plotGraph() {
         dataFile.close();
 
         TGraph *graph = new TGraph(x.size(), &y[0], &x[0]);
-        graph->SetTitle((filenames[i] + "@20.0 mK;I_Bol(A);V_Bol(V)").c_str());
+        graph->SetTitle((filenames[i] + "@" + temperature + ";I_Bol(A);V_Bol(V)").c_str());
         graph->SetMarkerStyle(20);
         graph->GetYaxis()->SetTitleOffset(1.30);
 
         // Define the function and fit it to the graph
-        TF1 *f1 = new TF1(("f" + filenames[i]).c_str(), "[0]*x + [1]", 0, 3e-10); // adjust the range as needed
+        TF1 *f1 = new TF1(("f" + filenames[i]).c_str(), "[0]*x + [1]", 0, 3e-9); // adjust the range as needed
 
 ///FIXME: before fitting
 
         switch (i)
         {
         case 0: //3x3NTD
-            f1->SetRange(0,1.8e-10);
+            //f1->SetRange(0,1.8e-10);//20.0mK
+            //f1->SetRange(0,3.34e-10);//22.2mK
             break;
         case 1: //3x1NTD
-            f1->SetRange(0,1.55e-10);
+            //f1->SetRange(0,1.55e-10);//20.0mK
+            //f1->SetRange(0,2.16e-10);//22.2mK
+            f1->SetRange(0,1.5e-9);//29.9mK
             break;
         case 2: //19T20
-            f1->SetRange(0,2.15e-10);
+            //f1->SetRange(0,2.15e-10);//20.0mK
+            //f1->SetRange(0,3.83e-10);//22.2mK
             break;
         
         default:
@@ -85,10 +98,10 @@ void plotGraph() {
         // Save the canvas to the ROOT file
         c1->Write();
         // Save the canvas to the PDF file
-        c1->Print("../output/2023Nov/20p0mK/allGraphs.pdf"); // Add the canvas to the PDF file
+        c1->Print((outputPath+"allGraphs.pdf").c_str()); // Add the canvas to the PDF file
     }
 
-    cAll->Print("../output/2023Nov/20p0mK/allGraphs.pdf]"); // Close the PDF file
+    cAll->Print((outputPath+"allGraphs.pdf]").c_str()); // Close the PDF file
 
     file->Close();
 }
