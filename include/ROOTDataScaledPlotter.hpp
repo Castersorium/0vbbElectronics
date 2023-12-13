e316d3f78dbb2e2edbe91bedf1dca7a6c7ff47ee
JeasonCao <cjx1999714@sina.cn>
2023-12-13 21:19:17 +0800

    Create ROOTDataScaledPlotter.hpp
    Creat re scaled plotter

---
+++ include/ROOTDataScaledPlotter.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef ROOTDATASCALEDPLOTTER_HPP
+#define ROOTDATASCALEDPLOTTER_HPP
+#include "ROOTDataPlotter.hpp"
+namespace TTREEIO
+{
+class ROOTDataScaledPlotter
+: public ROOTDataPlotter
+{
+
+};
+} // namespace TTREEIO
+
+
+#endif // ROOTDATASCALEDPLOTTER_HPP
