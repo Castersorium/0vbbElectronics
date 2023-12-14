7c5e8fac5f56cfd5ded073e21e0fed7d5e428f33
Castersorium <xzyu18@fudan.edu.cn>
2023-12-14 17:53:19 +0800

    Add fit y=kx, result not good

--- macro/EasyDraw.cpp
+++ macro/EasyDraw.cpp
@@ -1,12 +1,16 @@
 #include <TFile.h>
 #include <TTree.h>
 #include <TGraph.h>
+#include <TF1.h>
+#include <TStyle.h>
 #include <TCanvas.h>
 #include <iostream>
 #include <string>
 
 
 void EasyDraw(){
+    gStyle->SetOptFit(111);
+
     // Open the ROOT file
     std::string dataPath = "../data/";
     std::string outputPath = "../output/";
@@ -25,22 +29,49 @@ void EasyDraw(){
         return;
     }
 
+    // Fit a linear function to the data
+    TF1* linearFit = new TF1("linearFit", "[0]*x+[1]", 10e1, 10e4);
+    linearFit->SetLineColor(kRed);
+    TF1* linearFit2 = new TF1("linearFit2", "[0]*x+[1]", 10e1, 10e4);
+    linearFit2->SetLineColor(kRed);
+
     TFile* outFile = TFile::Open((outputPath+"testJYCanvas.root").c_str(),"RECREATE");
 
-    Int_t beforeGraph9_12 = tree2->Draw("ResistanceCh9:ResistanceCh12","ResistanceCh10<1e8&&ResistanceCh10>0&&ResistanceCh9<1e8&&ResistanceCh9>0","goff"); 
+    TCanvas *c1=new TCanvas("c1","c1",800,800);
+    Int_t beforeGraph9_12 = tree2->Draw("ResistanceCh9:ResistanceCh12","ResistanceCh12<1e10 && ResistanceCh12>0 && ResistanceCh9<1e10 &&    ResistanceCh9>0","goff"); 
     TGraph *graph9_12 = new TGraph(beforeGraph9_12, tree2->GetV2(), tree2->GetV1());
     graph9_12->SetNameTitle("ResistanceCh9:ResistanceCh12","ResistanceCh9:ResistanceCh12");
-    graph9_12->SetMarkerStyle(22); 
+    graph9_12->SetMarkerStyle(20); 
     graph9_12->Draw("AP");
+    graph9_12->Fit(linearFit,"RES+","",1, 10e4);
     graph9_12->Write();
+    gPad->SetLogx();
+    gPad->SetLogy();
+    c1->Draw();
 
-    Int_t beforeGraph7_11 = tree2->Draw("ResistanceCh7:ResistanceCh11","ResistanceCh7<1e10&&ResistanceCh7>0&&ResistanceCh11<1e10&&ResistanceCh11>0","goff"); 
+    TCanvas *c2=new TCanvas("c2","c2",800,800);
+    Int_t beforeGraph7_11 = tree2->Draw("ResistanceCh7:ResistanceCh11","ResistanceCh7<1e10 && ResistanceCh7>0 && ResistanceCh11<1e10 && ResistanceCh11>0","goff"); 
     TGraph *graph7_11 = new TGraph(beforeGraph7_11, tree2->GetV2(), tree2->GetV1()); 
     graph7_11->SetNameTitle("ResistanceCh7:ResistanceCh11","ResistanceCh7:ResistanceCh11");
-    graph7_11->SetMarkerStyle(22); 
+    graph7_11->SetMarkerStyle(20); 
     graph7_11->Draw("AP");
+    graph7_11->Fit(linearFit2,"RES+","",1, 10e4);
     graph7_11->Write();
-    //c1->SaveAs((outputPath+"testJYCanvas.root").c_str());
+    gPad->SetLogx();
+    gPad->SetLogy();
+    c2->Draw();
+
+    // TCanvas *c3=new TCanvas("ResistanceCh7:ResistanceCh11","ResistanceCh7:ResistanceCh11",800,800);
+    // Int_t beforeGraph7_11 = tree2->Draw("ResistanceCh7:ResistanceCh11","ResistanceCh7<1e10 && ResistanceCh7>0 && ResistanceCh11<1e10 && ResistanceCh11>0","goff"); 
+    // TGraph *graph7_11 = new TGraph(beforeGraph7_11, tree2->GetV2(), tree2->GetV1()); 
+    // graph7_11->SetNameTitle("ResistanceCh7:ResistanceCh11","ResistanceCh7:ResistanceCh11");
+    // graph7_11->SetMarkerStyle(20); 
+    // graph7_11->Draw("AP");
+    // graph7_11->Write();
+    // gPad->SetLogx();
+    // gPad->SetLogy();
+    // c3->Draw();
+
     outFile->Close();
 }     
 
