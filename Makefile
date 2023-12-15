cc42e3e4f4d0785c5e3aae3e646a63cf10a6e323
Castersorium <xzyu18@fudan.edu.cn>
2023-12-16 01:44:41 +0800

    could ignore checking and running the py script with -nex

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-CFLAGS = -Wall -g
-RT_CONFIG = `root-config --cflags --glibs`
+CXX := g++
+DEBUG_FLAGS := -g
+RELEASE_FLAGS := -O2 # Could be adjust
+
+# Default
+CXXFLAGS := -std=c++17 -Wall $(RELEASE_FLAGS)
+
+RT_CONFIG := `root-config --cflags --glibs`
 
 INCLUDE_PATH := ./include
 SOURCE_PATH := ./src/plotRTrecording
@@ -9,10 +15,14 @@ SOURCE := $(wildcard $(SOURCE_PATH)/*.cpp)
 TARGET := $(TARGET_PATH)/plotRTrecording.exe
 
 $(TARGET): $(SOURCE) | $(TARGET_PATH) # "|" makes sure target path is valid
-	g++ -std=c++17 -I$(INCLUDE_PATH)/ $(SOURCE) $(RT_CONFIG) -o $(TARGET) $(CFLAGS)
+	$(CXX) $(CXXFLAGS) -I$(INCLUDE_PATH)/ $(SOURCE) $(RT_CONFIG) -o $(TARGET) $(CFLAGS)
 
 $(TARGET_PATH):
 	mkdir -p $(TARGET_PATH)
+
+# For debug
+debug: CXXFLAGS += $(DEBUG_FLAGS)
+debug: $(TARGET)
 
 .PHONY: clean
 clean:
--- src/electronicsData/readXLSdraw.sh
+++ src/electronicsData/readXLSdraw.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-
 # Check if 'uproot' is installed
-if  pip show uproot &> /dev/null;
-then
-    echo "Uproot is installed, running program..."
-    echo
-    python extractXLS.py
-else
-    echo "Uproot is not installed, please try:"
-    echo "pip intall uproot"
-    echo "to continue to read the configuration file from excel"
+check_uproot(){
+    if  pip show uproot &> /dev/null;
+    then
+        echo "Uproot is installed, running program..."
+        echo
+        python extractXLS.py
+    else
+        echo "Uproot is not installed, please try:"
+        echo "pip intall uproot"
+        echo "This could to read the configuration file from excel"
+        exit 1
+    fi
+}
+
+if [ "$1" != "-nex" ]; then # nex = no need to extract
+    check_uproot
 fi
 
 echo "Now saving and drawing the VA Graph in a root file"
