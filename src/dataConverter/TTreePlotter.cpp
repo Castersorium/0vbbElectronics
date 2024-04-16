e644f0f405c5ecc37a485535dffa6e0662e799b4
Destiny_712 <576192564@qq.com>
2024-04-16 12:51:24 +0800

    Plotted amp.

--- src/dataConverter/TTreePlotter.cpp
+++ src/dataConverter/TTreePlotter.cpp
@@ -27,8 +27,38 @@ void TTreePlotter::createGraphFromTree( const std::string & rootFilePath, const 
         return;
     }
 
-    // 从TTree创建TGraphErrors
-    TGraphErrors * graph = new TGraphErrors( tree );
+    // 从TTree中获取数据
+    int n = tree->GetEntries();
+    double * x = new double[n];
+    double * y = new double[n];
+    double * ex = new double[n];  // x轴误差
+    double * ey = new double[n];  // y轴误差
+
+    double timestamp = 0;
+    double Amp_2cmLMO = 0;
+
+    tree->SetBranchAddress( "timestamp", &timestamp );
+    tree->SetBranchAddress( "2cmLMO", &Amp_2cmLMO );
+
+    for ( int i = 0; i < n; ++i )
+    {
+        tree->GetEntry( i );
+        x[i] = timestamp;
+        y[i] = Amp_2cmLMO;
+        // 假设x轴没有误差
+        ex[i] = 0;
+        // 假设y轴的误差是y的10%
+        ey[i] = y[i] * 0.1;
+    }
+
+    // 创建TGraphErrors
+    TGraphErrors * graph = new TGraphErrors( n, x, y, ex, ey );
+
+    // 注意：在使用完x和y后，记得删除它们
+    delete[] x;
+    delete[] y;
+    delete[] ex;
+    delete[] ey;
 
     // 将TGraphErrors保存到新的ROOT文件
     TFile * outputFile = TFile::Open( outputFilePath.c_str(), "RECREATE" );
