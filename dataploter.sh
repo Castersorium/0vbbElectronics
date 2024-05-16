23fe53d6ffc4547c230daee62e8a8681dc9f427c
JeasonCao <cjx1999714@sina.cn>
2024-05-16 15:11:50 +0800

    add SetMaxTreeSize to covert2TTree.cpp

--- dataploter.sh
+++ dataploter.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-make clean_DATACVT
+make clean
 sleep 2
 make dataConverter
-./build/dataConverter/dataConverter.exe ./data/NIDAQ_csv/Run2404/ ./output/ROOTdata/ ./output/plots/ ./data/BLUEFORS_LOG/ ./data/multimeterdata/Run2404/
+./build/dataConverter/dataConverter.exe ./data/NIDAQ_csv/Run2404/test/ ./output/ROOTdatatest/ ./output/plots/ ./data/BLUEFORS_LOG/ ./data/multimeterdata/Run2404/
