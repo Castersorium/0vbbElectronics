dae5ffc45aa68395a7ebb180d5de22197286c29b
Destiny_712 <576192564@qq.com>
2024-10-13 11:04:19 +0200

    Reapply "Prepared interface for R_Base, but not working."
    This reverts commit 00ad461448d2f58221fb0e6c2b6fe2e29977a52d.

--- include/flagHandler.hpp
+++ include/flagHandler.hpp
@@ -6,12 +6,13 @@
 #include <filesystem>
 
 void flagHandler(
-    const std::filesystem::path &NIDAQcsvDirPath,
-    const std::filesystem::path &rootDirPath,
-    const std::filesystem::path &plotDirPath,
-    const std::filesystem::path &BlueforsLogDirPath,
-    const std::filesystem::path &MultimeterDataDirPath,
-    const std::filesystem::path &CelsiusDataDirPath,
-    int argc, char *argv[]);
+    const std::filesystem::path & NIDAQcsvDirPath,
+    const std::filesystem::path & rootDirPath,
+    const std::filesystem::path & plotDirPath,
+    const std::filesystem::path & BlueforsLogDirPath,
+    const std::filesystem::path & MultimeterDataDirPath,
+    const std::filesystem::path & R_BaseDataDirPath,
+    const std::filesystem::path & CelsiusDataDirPath,
+    int argc, char * argv[] );
 
 #endif
